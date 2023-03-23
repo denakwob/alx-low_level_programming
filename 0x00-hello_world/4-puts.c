17f74264ee4709b5d4f361f794d825313cd9b87b
denakwob <nanaobengkweku@gmail.com>
2023-03-22 22:59:00 -0700

    Initial commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the content of puts
+ * 
+ * Return: 0 upon completion
+ *
+ * Author - Nana Obeng
+ */
+
+int main()
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return(0);
+}

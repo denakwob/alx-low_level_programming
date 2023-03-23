bf9f1cbdcdd45daf2555b37557939ac9e9e3f583
denakwob <nanaobengkweku@gmail.com>
2023-03-23 00:21:17 -0700

    Initial commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 /**
- * main - prints the string 
+ * main - prints the string
  *
  * Return: 1 upon success
  *
@@ -10,6 +10,7 @@
 int main(void)
 {
 	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(2, str, 59);
 	return (1);
 }

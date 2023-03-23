02f85ef9f9e4203ffff14555b5ab5c970e6db6ae
denakwob <nanaobengkweku@gmail.com>
2023-03-22 23:25:26 -0700

    Initial commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the function in printf
+ * Return: 0 upon sucessfu completion
+ *
+ * Author - Nana Obeng
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

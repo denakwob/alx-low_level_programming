8879bb37d45fa28309b31d857fcceb197929bdeb
denakwob <nanaobengkweku@gmail.com>
2023-03-23 00:18:33 -0700

    Initial commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - prints the string 
  *
@@ -9,8 +9,7 @@
 
 int main(void)
 {
-	char *str = "and that piece of art is useful" - Dora Korpar, 2015-10-19";
-	int len = strlen(str);
-	write(STDOUT_FILENO, str, len);
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, str, 38);
 	return (1);
 }

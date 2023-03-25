b97fc83026efef1d26ee6d115fee4239204991cd
denakwob <nanaobengkweku@gmail.com>
2023-03-25 15:21:36 -0700

    Initial commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,12 +10,11 @@ int main(void)
 {
 	int a;
 	int b;
-	
-	for ( a = 48; a <= 57; a++)
+	for (a = 48; a <= 57; a++)
 	{
 		putchar(a);
 	}
-	for ( b = 97; b <= 102; b++)
+	for (b = 97; b <= 102; b++)
 	{
 		putchar(b);
 	}

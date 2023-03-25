b20d45b7e2436965f32c05a375759f2e1721f7ea
denakwob <nanaobengkweku@gmail.com>
2023-03-25 14:54:02 -0700

    Initial commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print numbers 
+ *
+ * Return: 0 on success
+ *
+ * Author -Nana Obeng
+ */
+int main(void)
+{
+	int a;
+
+	for ( a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}

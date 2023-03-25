343ca99115fd6ed6970f12442e82094706aacac1
denakwob <nanaobengkweku@gmail.com>
2023-03-25 15:19:25 -0700

    Initial commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print hex in lowercase
+ *
+ * Return: 0 on success
+ *
+ * Author - Nana Obeng
+ */
+int main(void)
+{
+	int a;
+	int b;
+	
+	for ( a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	for ( b = 97; b <= 102; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}	

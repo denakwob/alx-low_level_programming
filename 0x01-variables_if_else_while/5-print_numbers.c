01ca4efbd8ed9bbb01bee8852e468ed1bb9b1b2c
denakwob <nanaobengkweku@gmail.com>
2023-03-25 14:45:17 -0700

    Initial commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print numbers in base 10
+ *
+ * Return: 0 on success
+ *
+ * Author - Nana Obeng
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}

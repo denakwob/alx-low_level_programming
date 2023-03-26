ac74bde381c520877771a55ae3d6c79cac368a8a
denakwob <nanaobengkweku@gmail.com>
2023-03-26 08:00:58 -0700

    Initial commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - prints two two digits combinations
+ *
+ * Return: 0 upon success
+ *
+ * Author - Nana Obeng
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 99; a++)
+	{
+		for (b = a; b <= 99; b++)
+		{
+			if (a < b)
+			{
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				putchar(' ');
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
+				if ( a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return 0;
+}

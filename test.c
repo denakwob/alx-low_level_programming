f9ed9153bbe9d43173863cfd7dbd9540ca56fc14
denakwob <nanaobengkweku@gmail.com>
2023-03-27 05:56:52 -0700

    Initial commit

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/** main - prints 
+ *
+ * Return: 0 on success
+ *
+ * Author: Nana Obeng
+ *
+ */
+int main(){
+	/* types go here */
+
+	/* declarations */
+
+	/* conditions or loops */
+}

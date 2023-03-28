393562ac19c613027a8c6bdf969aee5584abf82d
denakwob <nanaobengkweku@gmail.com>
2023-03-28 14:06:06 -0700

    C Nested loops project

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * int _islower - checks for lower case 
+ * @c: argument
+ *
+ * Return: on success 1
+ *
+ * Author - Nana Obeng
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+	return (1);
+}	
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,5 +3,6 @@
 #include <unistd.h>
 int _putchar(char c);
 void print_alphabet(void);
+int _islower(int c);
 void print_alphabet_x10(void);
 #endif

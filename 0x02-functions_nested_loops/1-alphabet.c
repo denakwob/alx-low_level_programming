899b95d6f498faa9cf88399f4410c9a5913850d2
denakwob <nanaobengkweku@gmail.com>
2023-03-28 13:23:16 -0700

    C Nested loops project

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabets in lower case
+ *
+ * Return: on success 1
+ *
+ * Author - Nana Obeng
+ */
+void print_alphabet(void)
+{
+	int num;
+	for (num = 97; num <= 122; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+	return;
+}

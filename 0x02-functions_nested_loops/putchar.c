8bec0f6a3d0329777131dc14baad34739ddf0e2e
denakwob <nanaobengkweku@gmail.com>
2023-03-28 12:16:32 -0700

    C Nested loops project

--- 0x02-functions_nested_loops/putchar.c
+++ 0x02-functions_nested_loops/putchar.c
@@ -1,21 +1,13 @@
-#include "main.h"
+#include <unistd.h>
+
 /**
- * main - prints _putchar
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-
-int main(void)
+int _putchar(char c)
 {
-	char str[] = "_putchar \n";
-	int i;
-	
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
-	return (0);
+	return (write(1, &c, 1));
 }

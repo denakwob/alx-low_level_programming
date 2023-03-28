0d4fc24d9cd2fade96ddd63f3324c91e768539f7
denakwob <nanaobengkweku@gmail.com>
2023-03-28 14:59:34 -0700

    C Nested loops project

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabets
+ * @c: argument
+ *
+ * Return: on success 1
+ *
+ * Author - Nana Obeng
+ */
+int _isalpha(int c)
+{
+	/* checks lowercase */
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	/* checks uppercase */
+	else if ( c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

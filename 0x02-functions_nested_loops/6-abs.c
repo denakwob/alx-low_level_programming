012519419a138680c4aa06056b47c349aa0029d8
denakwob <nanaobengkweku@gmail.com>
2023-03-28 15:31:21 -0700

    C Nested loops project

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - prints the absolute of a number
+ * @a: argument
+ *
+ * Return: 1 on success and 0 otherwise
+ *
+ * Author - Nana Obeng
+ */
+int _abs(int a)
+{
+	/* local variabe for an absolute declaration */
+	int absl;
+
+	if (a < 0)
+	{
+		absl = a * -1;
+	       return (abl);
+	}
+	return (absl)
+}	

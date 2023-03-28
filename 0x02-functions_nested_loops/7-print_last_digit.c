945968e1c18d742a97bff9ce2dbcc5149710b538
denakwob <nanaobengkweku@gmail.com>
2023-03-28 15:43:45 -0700

    C Nested loops project

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @a: argument
+ *
+ * Return: a on succces
+ *
+ * Author - Nana Obeng;
+ */
+int print_last_digit(int a)
+{
+	a = a % 10;
+
+	return (a);
+}

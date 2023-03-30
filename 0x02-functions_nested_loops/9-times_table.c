e8bb2c968523871a583ad094a8e204dc7fd678c6
denakwob <nanaobengkweku@gmail.com>
2023-03-29 23:20:28 -0700

    C Nested loops project

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * times_table - prints times table in 9
+ *
+ * Return: nothing
+ *
+ * Author - Nana Obeng
+ */
+void times_table(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			_putchar((a * b) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}

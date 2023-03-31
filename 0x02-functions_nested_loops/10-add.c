f82a0d58a61eae7a430af63881bf5fbbeafcaec9
denakwob <nanaobengkweku@gmail.com>
2023-03-31 02:12:51 -0700

    C Nested loops project

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two integer values
+ * @a - first argument
+ * @b - second argument
+ *
+ * Return: add
+ *
+ * Author - Nana Obeng
+ */
+int add(int a, int b)
+{
+	sum = a + b;
+	return sum;
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,11 +13,10 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b <= 9; b++)
-		{
-			_putchar((a * b) + '0');
-			_putchar(',');
-			_putchar(' ');
+		b = 9;
+		_putchar((a * b) + '0');
+		_putchar(',');
+		_putchar(' ');
 		}
 	}
 }

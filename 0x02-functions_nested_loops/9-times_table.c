b050200596a4de6e926b9e425c61151a2a648fd7
denakwob <nanaobengkweku@gmail.com>
2023-03-31 14:33:01 -0700

    C Nested loops project

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -34,13 +34,12 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(res / 10 + '0');
 				_putchar(res % 10 + '0');
-				_putchar(',');
 			}
 			/* If we're not on the last column, print a comma */
-		/*	if (b != 9)
+			if (b != 9)
 			{
 				_putchar(',');
-			}*/
+			}
 		}
 		/* Move to the next row */
 		_putchar('\n');

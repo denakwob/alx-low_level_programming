8d4b8ccc8df41812746fc0740da0766cdbdf89c9
denakwob <nanaobengkweku@gmail.com>
2023-03-31 03:56:08 -0700

    C Nested loops project

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,14 +10,27 @@ void times_table(void)
 {
 	int a;
 	int b;
+	int res;
 
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			_putchar((a * b) + '0');
-			_putchar(',');
-			_putchar(' ');
+			res = a * b;
+			if (res < 10)
+			{
+				_putchar(res + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(res / 10 + '0');
+				_putchar(res % 10 + '0');
+			}
+			if (b != 9)
+			{
+				_putchar(',');
+			}
 		}
 		_putchar('\n');
 	}

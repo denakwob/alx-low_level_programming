37a50c2da1264289c75d475685f5e618f82832fc
denakwob <nanaobengkweku@gmail.com>
2023-03-31 23:02:50 -0700

    C Nested loops project

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <unistd.h>
 char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+int _putchar(char c);
 #endif

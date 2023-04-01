f3c6d90ff340a38690fe4b2c86b369a245dfce18
denakwob <nanaobengkweku@gmail.com>
2023-04-01 12:48:23 -0700

    C Nested loops project

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,13 @@
 #include <unistd.h>
 char *rot13(char *);
 void reverse_array(int *a, int n);
+char *leet(char *c);
+char *cap_string(char *c);
+char *string_toupper(char *str);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);

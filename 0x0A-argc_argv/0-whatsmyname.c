accff3f910b95c10452d58b37199221e3534044b
denakwob <nanaobengkweku@gmail.com>
2023-10-29 08:18:31 -0700

    feat: print out name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of programme
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: 0 upon success
+ * Author - Nana Obeng
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 0)
+	{
+		printf("%s\n", argv[0]);
+		return (1);
+	}
+	else
+	{
+		printf("%s\n", argv[0]);
+		return (1);
+	}
+	return (0);
+}

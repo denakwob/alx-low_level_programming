6c85d50167d14df2344750a229d5b47717980751
denakwob <nanaobengkweku@gmail.com>
2023-10-29 16:58:21 -0700

    feat: print number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the argument count
+ * @argc: number of arguments
+ * @argv: array
+ *
+ * Return: 0 upon success
+ * Author - Nana Obeng
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}

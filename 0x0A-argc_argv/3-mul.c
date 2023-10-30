4dbd185989d7cc5acdb7e0a8fdf719113ee98e9e
denakwob <nanaobengkweku@gmail.com>
2023-10-29 17:39:15 -0700

    feat: multiply arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - performs multiplication
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: 0 upon success
+ * Author - Nana Obeng
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	long mul = 1, num;
+	char *ptr;
+
+	if (argc > 2)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			num = strtol(argv[i], &ptr, 10);
+			mul *= num;
+		}
+		printf("%ld\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}

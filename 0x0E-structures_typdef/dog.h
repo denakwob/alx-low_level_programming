341400d473d8968472b17f928200a4c53dd891f7
denakwob <nanaobengkweku@gmail.com>
2023-11-07 03:41:20 -0800

    Betty fix

--- 0x0E-structures_typdef/dog.h
+++ 0x0E-structures_typdef/dog.h
@@ -2,6 +2,13 @@
 #define DOG_H
 #include <stdio.h>
 
+/**
+ * struct dog - declare a type dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
 struct dog {
 	char *name;
 	float age;

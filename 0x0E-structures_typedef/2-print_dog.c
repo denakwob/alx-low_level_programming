071586b3938f0f52a494a2ee1bee3840ff383137
denakwob <nanaobengkweku@gmail.com>
2023-11-07 21:32:10 -0800

    fix: check wether d is NULL or not

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,21 +7,26 @@
 
 void print_dog(struct dog *d)
 {
-	if (d->name)
+	if (d)
 	{
-		printf("Name: %s\n", d->name);
-	}
-	else
-	{
-		printf("Name: (nil)\n");
-	}
-	printf("Age: %f\n", d->age);
-	if (d->owner)
-	{
-		printf("Owner: %s\n", d->owner);
-	}
-	else
-	{
-		printf("Owner: (nil)");
+		if (d->name)
+		{
+			printf("Name: %s\n", d->name);
+		}
+		else
+		{
+			printf("Name: (nil)\n");
+		}
+
+		printf("Age: %f\n", d->age);
+
+		if (d->owner)
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+		else
+		{
+			printf("Owner: (nil)");
+		}
 	}
 }

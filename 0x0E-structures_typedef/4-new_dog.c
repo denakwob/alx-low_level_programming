2c630d7e88663230e25d122912067ac9f3bb941a
denakwob <nanaobengkweku@gmail.com>
2023-11-07 21:54:50 -0800

    testing fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,26 +17,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	d = malloc(sizeof(dog_t));
 
-	if (d)
+	if (d == NULL)
 	{
-		d->name = name;
-		if (d->name == NULL)
-		{
-			free(d);
-			return (NULL);
-		}
-
-		d->age = age;
-		d->owner = owner;
-		if (d->owner == NULL)
-		{
-			free(d->name);
-			free(d);
-			return (NULL);
-		}
+		return (NULL);
 	}
-	else
+
+	d->name = strdup(name);
+
+	if (d->name == NULL)
 	{
+		free(d);
+		return (NULL);
+	}
+
+	d->age = age;
+
+	d->owner = strdup(owner);
+
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
 		return (NULL);
 	}
 	return (d);

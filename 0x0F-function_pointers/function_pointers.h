70b7f22355724439afea81b715f5b484892526a0
denakwob <nanaobengkweku@gmail.com>
2023-11-09 01:31:46 -0800

    fix to header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,7 @@
 #ifndef FUNC_H
 #define FUNC_H
-
+#include <stdio.h>
+#include <stdlib.h>
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
-int _strlen(char *string);
-//void f(char *name);
-
 #endif

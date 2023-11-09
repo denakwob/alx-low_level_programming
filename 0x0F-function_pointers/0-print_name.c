bff96ff1e565f3d430636f9b02dabcda9a0f2f6d
denakwob <nanaobengkweku@gmail.com>
2023-11-09 00:32:00 -0800

    testing

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,9 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints the name passed to it
- * @name: the name
- * @f: the function
- *
+ * print_name - prints a name
+ * @name: name passed
+ * @f: function
  * Author - Nana Obeng
  */
 
@@ -12,44 +11,6 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
 	{
-		f(name);
+		f = &name;
 	}
 }
-
-/**
- * f - prints the name passed to it
- * @name: name
-
-
-void f(char *name)
-{
-	int i;
-	int n;
-
-	n = _strlen(name);
-
-	for (i = 0; i < n; i++)
-	{
-		_putchar(name[i]);
-	}
-	_putchar('\n');
-}
-
-/**
- * _strlen - returns the len of a string
- * @string: string
- *
- * Return: count
-
-int _strlen(char *string)
-{
-	int count;
-
-	count = 0;
-	while (string[count] != '\0')
-	{
-		count++;
-	}
-	return (count);
-}
-*/

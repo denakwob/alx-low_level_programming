e292c3fd48e636d9fdedee3bdad65c6c0820859c
denakwob <nanaobengkweku@gmail.com>
2023-11-09 04:30:55 -0800

    Betty code style fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -19,15 +19,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	}
 
-	if (array && size && cmp)
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
+		if (cmp(array[i]) != 0)
 		{
-			if (cmp(array[i]) != 0)
-			{
-				return (i);
-			}
+			return (i);
 		}
-		return (-1);
 	}
+	return (-1);
 }

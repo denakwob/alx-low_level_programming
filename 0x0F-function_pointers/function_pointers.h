21fd8d611c8a270ebfd6fe7d04cda1b59de9e3a2
denakwob <nanaobengkweku@gmail.com>
2023-11-09 03:45:10 -0800

    function that formats an arrraya nd print the elements

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define FUNC_H
 #include <stdio.h>
 #include <stdlib.h>
+void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 #endif

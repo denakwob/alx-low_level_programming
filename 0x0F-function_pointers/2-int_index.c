1c7adafa3c06fe5bcd3f85dd86fb7f34b18cbca4
denakwob <nanaobengkweku@gmail.com>
2023-11-09 04:26:35 -0800

    C90 mixed declaration problem fixed

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,16 +12,16 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
+
 	if (size <= 0)
 	{
 		return (-1);
 	}
 
-	int i;
-
 	for (i = 0; i < size; i++)
 	{
-		if(cmp(array[i]))
+		if(cmp(array[i]) != 0)
 		{
 			return (i);
 		}
